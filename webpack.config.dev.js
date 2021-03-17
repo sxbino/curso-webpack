6f4d410c34ffb2db726081eff363ba98f495452b
Sabino Fernández <sabinofernandezo@gmail.com>
2021-03-17 11:28:11 -0500

    js portfolio

---
+++ webpack.config.dev.js
@@ -0,0 +1,86 @@
+const path = require('path'); // Para trabajar con archivos y rutas de directorios
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  stats: {
+    children: true,
+  },
+  entry: './src/index.js', // el punto de entrada de mi aplicación
+  output: { // esta es la salida de bundle
+    path: path.resolve(__dirname, 'dist'),
+    // resolve lo que hace es darnos la ruta absoluta de el S.O hasta nuestro archivo
+    // para no tener conflictos entre Linux, Windows, etc
+    filename: '[name].[contenthash].js',
+    // EL NOMBRE DEL ARCHIVO FINAL
+    assetModuleFilename: 'assets/images/[hash][ext][query]'
+  },
+  mode: 'development',
+  watch: true,
+  resolve: {
+    extensions: ['.js'], // LOS ARCHIVOS QUE WEBPACK VA A LEER
+    alias: {
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@templates': path.resolve(__dirname, 'src/templates/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        // Test declara que etensión de archivos aplicará el loader
+        test: /\.m?js$/,
+        // exclude permite omitir archivos o carpetas especificas
+        exclude: /node_modules/,
+        // Use es un arreglo u objeto donde expecificas que loader aplicaras
+        use: {
+          loader: 'babel-loader'
+        }
+      }, 
+      {
+        test: /\.s?css$/,
+        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ],
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: 'application/font-woff',
+            name: '[name].[ext]',
+            outputPath: './assets/fonts/',
+            publicPath: '../assets/fonts/',
+            esModule: false,
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[contenthash].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', 'assets/images'),
+          to: 'assets/images'
+        }
+      ]
+    }),
+    new Dotenv(),
+  ],
+};
---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+const path = require('path'); // Para trabajar con archivos y rutas de directorios
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizaerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  stats: {
+    children: true,
+  },
+  entry: './src/index.js', // el punto de entrada de mi aplicación
+  output: { // esta es la salida de bundle
+    path: path.resolve(__dirname, 'dist'),
+    // resolve lo que hace es darnos la ruta absoluta de el S.O hasta nuestro archivo
+    // para no tener conflictos entre Linux, Windows, etc
+    filename: '[name].[contenthash].js',
+    // EL NOMBRE DEL ARCHIVO FINAL
+    assetModuleFilename: 'assets/images/[hash][ext][query]'
+  },
+  mode: 'production',
+  resolve: {
+    extensions: ['.js'], // LOS ARCHIVOS QUE WEBPACK VA A LEER
+    alias: {
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@templates': path.resolve(__dirname, 'src/templates/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        // Test declara que etensión de archivos aplicará el loader
+        test: /\.m?js$/,
+        // exclude permite omitir archivos o carpetas especificas
+        exclude: /node_modules/,
+        // Use es un arreglo u objeto donde expecificas que loader aplicaras
+        use: {
+          loader: 'babel-loader'
+        }
+      }, 
+      {
+        test: /\.s?css$/,
+        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ],
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: 'application/font-woff',
+            name: '[name].[ext]',
+            outputPath: './assets/fonts/',
+            publicPath: '../assets/fonts/',
+            esModule: false,
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[contenthash].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', 'assets/images'),
+          to: 'assets/images'
+        }
+      ]
+    }),
+    new Dotenv(),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizaerPlugin(),
+      new TerserPlugin(),
+    ]
+  }
+};
